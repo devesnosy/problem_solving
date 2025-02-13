2390a4a3b4dca0bcea6195b30262202f0a21cdf8
Eyad Senosy <dev.esnosy@gmail.com>
2025-02-13 18:05:11 +0200

    1703A - YES or YES?

---
+++ yes_or_yes.py
@@ -0,0 +1,10 @@
+# https://codeforces.com/problemset/problem/1703/A
+
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    if s.lower() == "yes":
+        print("YES")
+    else:
+        print("NO")
